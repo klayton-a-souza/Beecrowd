ebb13ed40c91600ca9119c07d1c7f6ad7af730a9
klayton-a-souza <klayton.al.souza@gmail.com>
2022-07-21 14:58:30 -0300

    Solução problema 1002

---
+++ Iniciante/Java/1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/*
+ * Link da questão: https://www.beecrowd.com.br/judge/pt/problems/view/1003
+ */
+
+public class Uri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int SOMA = A+B;
+
+        System.out.println("SOMA = "+SOMA);
+
+    }
+}
