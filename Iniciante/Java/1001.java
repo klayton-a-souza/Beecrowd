627c9dcf6ea2fff9fe78171dd1cf77c1eac78b38
klayton-a-souza <klayton.al.souza@gmail.com>
2022-07-21 13:52:23 -0300

    Solução problema 1001

---
+++ Iniciante/Java/1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*
+ * Link da questão: https://www.beecrowd.com.br/judge/pt/problems/view/1001
+ */
+
+public class Uri {
+    public static void main(String[] args) {        
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int X = A+B;
+        System.out.println("X = " + X);
+    }
+}
